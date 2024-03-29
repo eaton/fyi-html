c3a815a412e83db317dce1a83293f62cb1260089
Jeff Eaton <jeff@eaton.fyi>
2024-03-28 22:04:01 -0500

    Extreme jank wins the day

--- src/html/extract/index.ts
+++ src/html/extract/index.ts
@@ -1,25 +1,31 @@
-import { cheerioJsonMapper, JsonTemplateObject, Options } from 'cheerio-json-mapper';
+import { cheerioJsonMapper, JsonTemplateObject, JsonTemplate, Options } from 'cheerio-json-mapper';
 import { z } from 'zod';
 import { pipeFnMap } from './pipes.js';
 
-export { type JsonTemplateObject } from 'cheerio-json-mapper';
+export { type JsonTemplateObject, type JsonTemplate } from 'cheerio-json-mapper';
+
+type MappedReturn<T extends string | JsonTemplateObject[] | JsonTemplateObject, S> =
+  (T extends string ? unknown : (T extends JsonTemplateObject[] ? S[] : S));
+
+const fallbackSchema = z.record(z.unknown());
 
 /**
  * Uses cheerio to extract structured data from markup
  */
-export async function extract<T extends z.ZodTypeAny>(
+export async function extract<T extends string | JsonTemplate, S extends z.ZodTypeAny = typeof fallbackSchema>(
   input: string | Buffer,
-  template: JsonTemplateObject | JsonTemplateObject[],
-  schema: T,
+  template: T,
+  schema?: S,
   options: Partial<Options> = {}
-): Promise<z.infer<T>> {
-
+): Promise<MappedReturn<T, z.infer<S>>> {
   if (options) {
     options.pipeFns = { ...options.pipeFns, ...pipeFnMap};
   } else {
     options = { pipeFns: pipeFnMap };
   }
-  
+
+  const parsingSchema = schema ?? (Array.isArray(template) ? z.array(fallbackSchema) : fallbackSchema);
+
   return cheerioJsonMapper(input.toString(), template, options)
-    .then(results => schema.parse(results))
+    .then(results => (schema ?? parsingSchema).parse(results))
 }
